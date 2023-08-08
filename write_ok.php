4c100be52dd49706fcd18293a2f93cbb24c52122
테스트 <test@gmail.com>
2023-08-08 16:18:53 +0900

    080804

--- write_ok.php
+++ write_ok.php
@@ -1,9 +1,9 @@
 <?php
 include "dbcon.php";
 
+$userid="hong";//userid는 없어서 임의로 넣어줬다.
 $subject=$_POST["subject"];
 $content=$_POST["content"];
-$userid="hong";//userid는 없어서 임의로 넣어줬다.
 $status=1;//status는 1이면 true, 0이면 false이다.
 
 $sql="INSERT INTO board (userid,subject,content) VALUES ('".$userid."','".$subject."','".$content."')";
