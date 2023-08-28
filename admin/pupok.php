d5f5c1baee98b8931ca4e7b764bd1cd8fc7f489e
테스트 <test@gmail.com>
2023-08-28 15:19:28 +0900

    082802

---
+++ admin/pupok.php
@@ -0,0 +1,93 @@
+<?php session_start();
+include "/var/www/html/dbcon.php";
+ini_set( 'display_errors', '0' );
+
+if(!$_SESSION['AUID']){
+    echo "<script>alert('권한이 없습니다.');history.back();</script>";
+    exit;
+}
+
+
+$cate=$_POST["cate1"].$_POST["cate2"].$_POST["cate3"];//대중소분류를 모두 저장한다.
+$name=$_POST["name"];//제품명
+$delivery_fee=$_POST["delivery_fee"];//택배비
+$price=$_POST["price"];//가격
+$sale_price=$_POST["sale_price"];//세일가
+$sale_ratio=$_POST["sale_ratio"];//세일비율
+$cnt=$_POST["cnt"];//재고
+$contents=rawurldecode($_POST['contents']);//제품 설명
+$ismain=$_POST["ismain"];//메인
+$isnew=$_POST["isnew"];//신상품
+$isbest=$_POST["isbest"];//베스트
+$isrecom=$_POST["isrecom"];//추천
+$locate=$_POST["locate"];//위치
+$sale_end_date=$_POST["sale_end_date"];//판매종료일
+$file_table_id=$_POST["file_table_id"];//이미지
+$file_table_id=rtrim($file_table_id,",");//오른쪽 끝에 , 삭제
+
+if($_FILES["thumbnail"]["name"]){//첨부한 파일이 있으면
+
+        if($_FILES['thumbnail']['size']>10240000){//10메가
+            echo "<script>alert('10메가 이하만 첨부할 수 있습니다.');history.back();</script>";
+            exit;
+        }
+
+        if($_FILES['thumbnail']['type']!='image/jpeg' and $_FILES['thumbnail']['type']!='image/gif' and $_FILES['thumbnail']['type']!='image/png'){//이미지가 아니면, 다른 type은 and로 추가
+            echo "<script>alert('이미지만 첨부할 수 있습니다.');history.back();</script>";
+            exit;
+        }
+
+        $save_dir = "/var/www/html/data/";//파일을 업로드할 디렉토리
+        $filename = $_FILES["thumbnail"]["name"];
+        $ext = pathinfo($filename,PATHINFO_EXTENSION);//확장자 구하기
+        $newfilename = date("YmdHis").substr(rand(),0,6);
+        $thumbnail = $newfilename.".".$ext;//새로운 파일이름과 확장자를 합친다
+       
+        if(move_uploaded_file($_FILES["thumbnail"]["tmp_name"], $save_dir.$thumbnail)){
+            $thumbnail = "/var/www/html/data/".$thumbnail;
+        }else{
+            echo "<script>alert('이미지를 등록할 수 없습니다. 관리자에게 문의해주십시오.');history.back();</script>";
+            exit;
+        }
+
+}
+
+$sale_cnt = 0;//판매량
+$query="INSERT INTO products
+(name, cate, content, thumbnail, price, sale_price, sale_ratio, cnt, sale_cnt, isnew, isbest, isrecom, ismain, locate, userid, sale_end_date, reg_date, delivery_fee)
+VALUES('$name'
+, '".$cate."'
+, '".$contents."'
+, '".$thumbnail."'
+, '".$price."'
+, '".$sale_price."'
+, '".$sale_ratio."'
+, ".$cnt."
+, ".$sale_cnt."
+, '".$isnew."'
+, '".$isbest."'
+, '".$isrecom."'
+, '".$ismain."'
+, '".$locate."'
+, '".$_SESSION['AUID']."'
+, '".$sale_end_date."'
+, now()
+, '".$delivery_fee."'
+)";
+
+$rs=$mysqli->query($query) or die($mysqli->error);
+$pid = $mysqli -> insert_id;
+if($rs){
+
+    $upquery="update product_image_table set pid=".$pid." where imgid in (".$file_table_id.")";
+    $fs=$mysqli->query($upquery) or die($mysqli->error);
+    echo "<script>alert('등록했습니다.');location.href='/admin/product_list.php';</script>";
+    exit;
+
+}else{
+    echo "<script>alert('등록하지 못했습니다. 관리자에게 문의해주십시오.');history.back();</script>";
+    exit;
+}
+
+
+?>
