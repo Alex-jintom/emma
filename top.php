8b68a4934a62e2297739c76449d1d8d06ce4a301
테스트 <test@gmail.com>
2023-09-04 18:40:10 +0900

    090410

--- top.php
+++ top.php
@@ -124,7 +124,7 @@ function islanguage($n){
                         <li class="active"><a href="/">Home</a></li>
                         <li><a href="shop.html">Shop page</a></li>
                         <li><a href="single-product.html">Single product</a></li>
-                        <li><a href="cart.php">Cart</a></li>
+                        <li><a href="">Cart</a></li>
                         <li><a href="checkout.html">Checkout</a></li>
                         <li><a href="#">Category</a></li>
                         <li><a href="#">Others</a></li>
